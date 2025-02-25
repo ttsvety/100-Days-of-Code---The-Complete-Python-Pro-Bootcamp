1c730472613ac29b7a237e4938fcf02c9681f210
Tsvety <19126@uktc-bg.com>
2025-02-25 17:08:31 +0200

    Day1

---
+++ Day1/2.String Manipulation/task.py
@@ -0,0 +1,5 @@
+# Write your code below this line 👇
+print("Hello world!\nHello World!\nHello World!")
+print("Hello " + "Tsvetina")
+print("Hello" + " Tsvetina")
+print("Hello" + " " + "Tsvetina")
---
+++ Day1/5.Band Name Generator/task.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is your pet's name?\n")
+print("Your band name could be: " + city + " " + pet)
